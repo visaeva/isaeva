f3fa35416407647d6f93ff3ff5b36a328bd8a4e0
Victoria Isaeva <visaeva0302@gmail.com>
2023-03-14 16:02:55 +0300

    В проекте создана кнопка и лейбл. При нажатии на кнопку - меняется значение счетчика в лейбле

--- isaeva/ViewController.swift
+++ isaeva/ViewController.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isLabel = 0
 
+    @IBOutlet weak var LabelCount: UILabel!
+    
+    @IBAction func ButtonCounter(_ sender: Any) {
+isLabel = isLabel + 1
+        LabelCount.text = String(isLabel)
+    }
+
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        LabelCount.text = String(isLabel)
         // Do any additional setup after loading the view.
     }
 
